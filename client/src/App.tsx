64ec5daeb959391d7d4d999d71d6203421b8de72
Ciano Dev <lorencianodramayo@gmail.com>
2022-03-27 16:09:32 +0800

    updated home content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
-import { Layout, Menu, Breadcrumb, PageHeader, Button } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import React, { FC } from 'react';
+import { Layout, PageHeader, Button, Card, Row, Col, Typography, Input  } from 'antd';
+
+import docuSign from './assets/docu_sign.png';
+import armis from './assets/armis.png';
+import intrado from './assets/intrado.png';
+import siemens from './assets/siemens.png';
+import blueGrace from './assets/blue_grace.png';
+
 import './App.less';
 
-const { SubMenu } = Menu;
-const { Header, Content, Footer, Sider } = Layout;
+const { Title } = Typography;
+const { Header, Content, Footer } = Layout;
+const { Search } = Input;
 
-function App() {
+const App:FC = () => {
   return (
     <Layout className="App">
       <Header className="App-header"> 
@@ -20,10 +28,81 @@ function App() {
           ]}
         />
       </Header>
-      <Content style={{ padding: '0 50px' }}>
-        Text
+      <Content className="App-content">
+        {/* Banner  */}
+        <Row className="content-banner">
+          <Col span={24}>
+          <Card bordered={false}>
+            <Title level={2}>Tech content for today's fast paced world</Title>
+            <p>Martechgrp is a leading Global platform for sourcing the latest technology products and services available.</p>
+            <Button type="primary">
+              Asset
+            </Button>
+          </Card>
+          </Col>
+        </Row>
+        {/* Brand  */}
+        <Row className="content-brand" gutter={[48, 0]}>
+          <Col span={4}>
+            <img src={docuSign} alt="Docu Sign" />
+          </Col>
+          <Col span={4}>
+           <img src={armis} alt="Armis" />
+          </Col>
+          <Col span={4}>
+            <img src={intrado} alt="Intrado" />
+          </Col>
+          <Col span={4}>
+            <img src={siemens} alt="Siemens" />
+          </Col>
+          <Col span={4}>
+            <img src={blueGrace} alt="Blue Grace" />
+          </Col>
+        </Row>
+        {/*  About */}
+        <Row className="content-about">
+          <Col>
+            <Card bordered={false}>
+              <Title level={4}>What we do</Title>
+              <Title level={3}>We guide tech buyers through their buying journey</Title>
+              <p>Martechgrp provides you with the latest trustworthy, educational &amp; informative content. We help you take the hassle out of sourcing in a one-stop easy-to-use platform allowing you to evaluate, examine and review the latest products and services a business needs within this fast-paced technology world.</p>
+              <p>Easy to navigate and access to a library of whitepapers, infographics, and webinars that are key components in the activities of influential IT decision makers around the world.</p>
+              <p>Technology research has never been so easy, so why not become a part of our successful online community.</p>
+            </Card>
+          </Col>
+        </Row>
+        {/* Industry  */}
+        <Row className="content-industry" gutter={[48, 48]}>
+          <Col span={8}><Button>SALES</Button></Col>
+          <Col span={8}><Button>MARKETINJG</Button></Col>
+          <Col span={8}><Button>FINANCE</Button></Col>
+          <Col span={8}><Button>ACCOUNTING</Button></Col>
+          <Col span={8}><Button>HR</Button></Col>
+          <Col span={8}><Button>IT</Button></Col>
+        </Row>
+        {/* Subscribe  */}
+        <Row className="content-subscribe">
+          <Col>
+            <Card bordered={false}>
+              <Title level={2}>Subscribe to get our latest content</Title>
+              <Search
+                placeholder="input search text"
+                allowClear
+                enterButton="Search"
+                size="large"
+              />
+            </Card>
+          </Col>
+        </Row>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      <Footer>
+        <Row>
+          <Col>1</Col>
+          <Col>2</Col>
+          <Col>3</Col>
+          <Col>4</Col>
+        </Row>
+      </Footer>
     </Layout>
   );
 }
